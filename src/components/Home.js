d38f22963c50786dfa1b8da7a133531178ac9cc4
Pedro Pablo Miron Pozo <ppmironpozo@gmail.com>
2018-02-02 01:17:08 +0100

    renamed missing logo

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
       <h2>Search for a movie!</h2>
       <SearchBar />
       <Footer>Created by Pedro Pablo Miron Pozo</Footer>
-      <TMDBLogo src={tmdbLogo} alt="logo" />
+      <TMDBLogo src={TMDBLogo} alt="logo" />
       <div>Icons made by <a href="https://www.flaticon.com/authors/business-dubai" title="Business Dubai">Business Dubai</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a></div>
     </div>
   );
